448e21300870b7e15191f114f196307c40df60f0
apoorve <apoorve.shukla.cse20@itbhu.ac.in>
2021-11-07 02:55:03 +0530

    Added axios functionality

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,37 @@
-import React from "react";
-import logo from "../images/logo.jpg"
+/* eslint-disable object-shorthand */
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-const login = () => {
+const Login = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      router.push("/");
+    }
+  }, []);
+
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const data = {
+    username: username,
+    // eslint-disable-next-line prettier/prettier
+    password: password,
+  };
+
+  const loginfunc = () => {
+    axios
+      .post(`http://127.0.0.1:8000/auth/login/`, data)
+      .then((response) => {
+        router.push("/");
+        localStorage.setItem("token", response.data.token);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -25,15 +55,18 @@ const login = () => {
         <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
           <form className="mb-0 space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email address
+              <label htmlFor="text" className="block text-sm font-medium text-gray-700">
+                username
               </label>
               <div className="mt-1">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="text"
+                  name="text"
+                  type="text"
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  autoComplete="username"
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
                 />
               </div>
@@ -49,6 +82,9 @@ const login = () => {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
                 />
               </div>
@@ -56,9 +92,12 @@ const login = () => {
 
             <div>
               <button
-                type="submit"
+                onClick={() => {
+                  loginfunc();
+                }}
+                type="button"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Sign up
+                Sign in
               </button>
             </div>
           </form>
@@ -68,4 +107,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
