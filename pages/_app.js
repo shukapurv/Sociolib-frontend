a04958fbaed91ce997f69d245504e393393942cd
tiger-yash <tigeryashraj@gmail.com>
2021-11-07 16:19:04 +0530

    updated app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 /* eslint-disable react/prop-types */
 import "../styles/globals.css";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../utils/theme/theme";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    AOS.init();
+  });
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default MyApp;
