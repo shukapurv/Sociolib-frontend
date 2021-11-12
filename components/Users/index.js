c6bd7c5a736d88d7bf9c1b58610576b76d8647c6
apoorve <apoorve.shukla.cse20@itbhu.ac.in>
2021-11-13 00:23:16 +0530

    Added search in users page

--- components/Users/index.js
+++ components/Users/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@material-ui/core";
 import { list } from "../../list";
 import User from "./user";
 
 const index = () => {
+  const [filterUsers, setFilterUsers] = useState(list);
+  const [searchKey, setSearchKey] = useState("");
+
+  React.useEffect(() => {
+    let result;
+    if (searchKey.length === 0) result = list;
+    else {
+      result = list.filter((data) => {
+        return data.title.search(searchKey) !== -1;
+      });
+    }
+    setFilterUsers(result);
+  }, [searchKey]);
+
   return (
     <div>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -33,9 +48,31 @@ const index = () => {
             our already registered users
           </h2>
         </div>
+        <Box m={13}>
+          <div className="font-sans text-black bg-white flex items-center justify-center">
+            <div className="border rounded overflow-hidden flex">
+              <input
+                type="text"
+                className="px-4 py-2"
+                placeholder="Search..."
+                onChange={(e) => setSearchKey(e.target.value)}
+                value={searchKey}
+              />
+              <button type="button" className="flex items-center justify-center px-4 border-l">
+                <svg
+                  className="h-4 w-4 text-grey-dark"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24">
+                  <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </Box>
         <div>
           <ul className="grid gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-3">
-            {list.map((a) => (
+            {filterUsers.map((a) => (
               <li key={a.id}>
                 <User />
               </li>
