4711d82fb4b42f799a7011c2dc22d05b2fb12f04
tiger-yash <tigeryashraj@gmail.com>
2021-11-14 15:22:27 +0530

    fix

--- components/Booklist/Card.js
+++ components/Booklist/Card.js
@@ -9,30 +9,30 @@ const Card = (props) => {
   const image = `https://sociolib-api.herokuapp.com${props.cover_image}`;
   return (
     <>
-      <article className="overflow-hidden rounded-lg shadow-lg">
-        <a href={route}>
+      <article className="overflow-hidden rounded-lg shadow-lg cursor-pointer">
+        <Link href={`/book/${props.id}`}>
           <img alt="Placeholder" className="block h-auto w-full" src={image} />
-        </a>
+        </Link>
 
-        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-          <h1 className="text-lg">
-            <a className="no-underline hover:underline text-black" href={route}>
-              {props.name}
-            </a>
-          </h1>
+        <header className="flex items-center justify-between leading-tight p-2 md:p-4  cursor-pointer">
+          <Link href={`/book/${props.id}`}>
+            <h1 className="text-lg">
+              <div className="no-underline hover:underline text-black">
+                {props.name}
+              </div>
+            </h1>
+          </Link>
         </header>
 
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-          <Link href={`/book/${props.id}`}>
-            <div className="flex items-center no-underline hover:underline text-black">
-              <img
-                alt="Placeholder"
-                className="block rounded-full"
-                src="https://picsum.photos/32/32/?write"
-              />
-              <p className="ml-2 text-sm">{props.author}</p>
-            </div>
-          </Link>
+          <div className="flex items-center no-underline hover:underline text-black">
+            <img
+              alt="Placeholder"
+              className="block rounded-full"
+              src="https://picsum.photos/32/32/?write"
+            />
+            <p className="ml-2 text-sm">{props.author}</p>
+          </div>
         </footer>
       </article>
     </>
