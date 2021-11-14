d67017816e3b401d20932a1619d50bdf835d56d1
tiger-yash <tigeryashraj@gmail.com>
2021-11-14 12:30:12 +0530

    profile completed

---
+++ components/profile/MyFriends.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "react-multi-carousel/lib/styles.css";
+import axios from "axios";
+import User from "@components/Users/user";
+
+const MyFriends = ({ token, username }) => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    axios
+      .post(
+        `https://sociolib-api.herokuapp.com/friends/all/`,
+        { username: username },
+        { headers: { Authorization: `Token ${token}` } },
+      )
+      .then((res) => {
+        console.log(res)
+        setFriends(res.data.friends)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [])
+  return <>
+    <div>
+      <ul className="grid gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-3">
+        {friends.map((a) => (
+          <li key={a.id}>
+            <User {...a} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>;
+};
+
+export default MyFriends;
--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Headers from "@components/Headers";
 import { Box, makeStyles } from "@material-ui/core";
 import { Helmet } from "react-helmet";
 import Footer from "@components/Footer";
 import { useRouter } from "next/router";
 import ProfileComp from "@components/profile/index";
+import Cookie from "js-cookie";
+import axios from "axios";
 
 const Profile = () => {
   const { query } = useRouter();
+  const [token, setToken] = useState(Cookie.get("Token") || null);
+  const router = useRouter();
+  const [activeUser, setActiveUser] = useState("");
+  const [user, setUser] = useState("");
+  const [self, setSelf] = useState(false);
+  const [friendData, setFriendData] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .post(
+          `https://sociolib-api.herokuapp.com/auth/profile/`,
+          { username: router.query.username },
+          { headers: { Authorization: `Token ${token}` } },
+        )
+        .then((res) => {
+          setUser(res.data);
+        })
+        .catch((err) => {
+          console.log(router.query.username);
+          console.error(err);
+        });
+
+      axios
+        .get(`https://sociolib-api.herokuapp.com/auth/profile/`, {
+          headers: { Authorization: `Token ${token}` },
+        })
+        .then((res) => {
+          setActiveUser(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      if (user && activeUser && user.username === activeUser.username) setSelf(true);
+      else {
+        axios
+          .get(
+            `https://sociolib-api.herokuapp.com/friends/${user.id}/`,
+            { headers: { Authorization: `Token ${token}` } },
+          )
+          .then((res) => {
+            console.log(res)
+            setFriendData(res.data)
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+    }
+  }, [token]);
+  useEffect(() => {
+    if (user && activeUser && user.username === activeUser.username) setSelf(true);
+  }, [user, activeUser]);
   return (
     <>
       <Helmet>
@@ -15,8 +70,10 @@ const Profile = () => {
         <meta name="description" content=" " />
         <meta name="keywords" content="  " />
       </Helmet>
-      <Headers />
-      <ProfileComp />
+      <div className="h-screen relative">
+        <Headers />
+        <ProfileComp self={self} user={user} friendData={friendData} token={token} />
+      </div>
       <Footer />
     </>
   );
